2da10e2c28e78deefdf46ccf9909685ca7648dcc
Gaherka <113412320+Gaherka@users.noreply.github.com>
2023-02-09 17:53:51 -0600

    feat: A new feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo1 from '../assets/img/logo1.svg'
+import logoK from '../assets/img/logoK.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -35,7 +35,7 @@ export const NavBar = () => {
             <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
                 <Container>
                     <Navbar.Brand href="/">
-                        <img src={logo1} alt="Logo" />
+                        <img src={logoK} alt="Logo" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
